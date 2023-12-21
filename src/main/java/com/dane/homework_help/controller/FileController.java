774a89b48509920eb388aca891920ecf6e2a0cc3
DanE4 <90055044+DanE4@users.noreply.github.com>
2023-12-21 20:38:38 +0100

    feat: implemented dynamic image compression [skip ci]

--- src/main/java/com/dane/homework_help/controller/FileController.java
+++ src/main/java/com/dane/homework_help/controller/FileController.java
@@ -3,11 +3,15 @@ package com.dane.homework_help.controller;
 
 import com.dane.homework_help.exception.StorageFileNotFoundException;
 import com.dane.homework_help.service.StorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,48 +29,55 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RequestMapping("/api/files")
 public class FileController {
-    	private final StorageService storageService;
+    private final StorageService storageService;
 
 
-	@GetMapping("/")
-	public String listUploadedFiles(Model model) throws IOException {
+    @GetMapping("/")
+    public String listUploadedFiles(Model model) throws IOException {
 
-		model.addAttribute("files", storageService.loadAll().map(
-				path -> MvcUriComponentsBuilder.fromMethodName(FileController.class,
-						"serveFile", path.getFileName().toString()).build().toUri().toString())
-				.collect(Collectors.toList()));
+        model.addAttribute("files", storageService.loadAll().map(
+                        path -> MvcUriComponentsBuilder.fromMethodName(FileController.class,
+                                "serveFile", path.getFileName().toString()).build().toUri().toString())
+                .collect(Collectors.toList()));
 
-		return "uploadForm";
-	}
+        return "uploadForm";
+    }
 
-	@GetMapping("/files/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-		Resource file = storageService.loadAsResource(filename);
+        Resource file = storageService.loadAsResource(filename);
 
-		if (file == null)
-			return ResponseEntity.notFound().build();
+        if (file == null)
+            return ResponseEntity.notFound().build();
 
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
-	}
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
 
-	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file,
-			RedirectAttributes redirectAttributes) {
+    @Operation(summary = "Upload a file",
+            description = "This operation allows you to upload a file. The file is then stored and a success message is returned.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
+                            schema = @Schema(type = "string", format = "binary", description = "The file to be uploaded"))
+            ))
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
 
-		storageService.store(file);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + file.getOriginalFilename() + "!");
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
 
-		return "redirect:/";
-	}
+        return "redirect:/";
+    }
 
-	@ExceptionHandler(StorageFileNotFoundException.class)
-	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-		return ResponseEntity.notFound().build();
-	}
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
 
 }
    /* public ResponseEntity<Response> uploadFile(@PathVariable(value = "user_id") UUID userId) {
--- src/main/java/com/dane/homework_help/service/impl/StorageServiceImpl.java
+++ src/main/java/com/dane/homework_help/service/impl/StorageServiceImpl.java
@@ -12,15 +12,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
+import javax.imageio.*;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.util.Iterator;
 import java.util.Objects;
 import java.util.stream.Stream;
+
 @Data
 @Slf4j
 @Service
@@ -29,84 +33,152 @@ public class StorageServiceImpl implements StorageService {
     private String location = "src/main/resources/uploads";
 
     private final Path rootLocation;
-	public StorageServiceImpl() {
-        if(this.getLocation().trim().isEmpty()){
+
+    public StorageServiceImpl() {
+        if (this.getLocation().trim().isEmpty()) {
             throw new StorageException("File upload location can not be Empty.");
         }
+        this.rootLocation = Paths.get(getLocation());
+    }
 
-		this.rootLocation = Paths.get(getLocation());
-	}
-    @Override
-	public void store(MultipartFile file) {
-		try {
-			if (file.isEmpty()) {
-				throw new StorageException("Failed to store empty file.");
-			}
-			Path destinationFile = this.rootLocation.resolve(
-					Paths.get(Objects.requireNonNull(file.getOriginalFilename())))
-					.normalize().toAbsolutePath();
-			if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
-				// This is a security check
-				throw new StorageException(
-						"Cannot store file outside current directory.");
-			}
-			try (InputStream inputStream = file.getInputStream()) {
-				Files.copy(inputStream, destinationFile,
-					StandardCopyOption.REPLACE_EXISTING);
-			}
-		}
-		catch (IOException e) {
-			throw new StorageException("Failed to store file.", e);
-		}
-	}
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = File.createTempFile("prefix", "suffix");
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
 
-	@Override
-	public Stream<Path> loadAll() {
-		try {
-			return Files.walk(this.rootLocation, 1)
-				.filter(path -> !path.equals(this.rootLocation))
-				.map(this.rootLocation::relativize);
-		}
-		catch (IOException e) {
-			throw new StorageException("Failed to read stored files", e);
-		}
+    private String getFormatName(File imageFile) throws IOException {
+        ImageInputStream iis = ImageIO.createImageInputStream(imageFile);
+        Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(iis);
 
-	}
-
-	@Override
-	public Path load(String filename) {
-		return rootLocation.resolve(filename);
-	}
-
-	@Override
-public Resource loadAsResource(String filename) {
-    try {
-        Path file = load(filename);
-        Resource resource = new UrlResource(file.toUri());
-
-        if (!resource.exists() || !resource.isReadable()) {
-            throw new StorageFileNotFoundException("Could not read or find file: " + filename);
+        if (!imageReaders.hasNext()) {
+            throw new IllegalArgumentException("Cannot determine format of image");
         }
 
-        return resource;
-    } catch (MalformedURLException e) {
-        throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+        ImageReader reader = imageReaders.next();
+        iis.close();
+        log.info("Format name: " + reader.getFormatName());
+        return reader.getFormatName();
     }
-}
+
+    public File compressImage(File inputImage, float quality, Path destinationPath) throws IOException {
+        InputStream inputStream = new FileInputStream(inputImage);
+        BufferedImage bufferedImage = ImageIO.read(inputStream);
+
+        Iterator<ImageWriter> imageWriters = ImageIO.getImageWritersByFormatName(getFormatName(inputImage));
+
+        if (!imageWriters.hasNext())
+            throw new IllegalStateException("Writers Not Found!!");
+
+        ImageWriter imageWriter = imageWriters.next();
+
+        // Create an in-memory ByteArrayOutputStream to hold the compressed image
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(byteArrayOutputStream);
+        imageWriter.setOutput(imageOutputStream);
+
+        ImageWriteParam imageWriteParam = imageWriter.getDefaultWriteParam();
+
+        // Set the compress quality metrics
+        imageWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        imageWriteParam.setCompressionQuality(quality);
+
+        // Created image
+        imageWriter.write(null, new IIOImage(bufferedImage, null, null), imageWriteParam);
+
+        // close all streams
+        inputStream.close();
+        imageOutputStream.close();
+        imageWriter.dispose();
+
+        // Convert compressed image in ByteArrayOutputStream to File
+        File compressedImageFile = new File(destinationPath.toString());
+        try (FileOutputStream fos = new FileOutputStream(compressedImageFile)) {
+            fos.write(byteArrayOutputStream.toByteArray());
+        }
+
+        return compressedImageFile;
+    }
+
+    @Override
+    public void store(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file.");
+            }
+
+            Path destination = this.rootLocation.resolve(
+                            Paths.get(Objects.requireNonNull(file.getOriginalFilename())))
+                    .normalize().toAbsolutePath();
+
+            if (!destination.getParent().equals(this.rootLocation.toAbsolutePath())) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file outside current directory.");
+            }
+
+            if (Objects.requireNonNull(file.getContentType()).contains("image")) {
+                log.info("Compressing image...");
+                File fileToCompress = convertMultiPartToFile(file);
+                File compressedFile = compressImage(fileToCompress, 0.5f, destination);
+                log.info("Compressed image stored at: " + destination.toString());
+            } else {
+                log.info("Storing non-image type file...");
+                // Handle storing non-image file if needed
+            }
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file.", e);
+        }
+    }
 
 
-	@Override
-	public void deleteAll() {
-		FileSystemUtils.deleteRecursively(rootLocation.toFile());
-	}
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(this.rootLocation::relativize);
+        } catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
 
-	@Override
-	public void init() {
-		try {
-			Files.createDirectories(rootLocation);
-		}
-		catch (IOException e) {
-			throw new StorageException("Could not initialize storage", e);
-		}
-	}
+    }
+
+    @Override
+    public Path load(String filename) {
+        return rootLocation.resolve(filename);
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        try {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (!resource.exists() || !resource.isReadable()) {
+                throw new StorageFileNotFoundException("Could not read or find file: " + filename);
+            }
+
+            return resource;
+        } catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+        }
+    }
+
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
+    }
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
+    }
 }
